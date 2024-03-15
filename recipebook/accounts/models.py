872c8e768d4fed6e6a1adb89c63b8cfb5b5dd84f
AvielleGregorio <sophia.gregorio@student.ateneo.edu>
2024-03-15 22:47:37 +0800

    Set up the model profile and admin of accounts

--- recipebook/accounts/models.py
+++ recipebook/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    shortBio = models.CharField(max_length=1000, validators=[MinLengthValidator(255)])
+    
+    def __str__(self):
+        return self.name
+
